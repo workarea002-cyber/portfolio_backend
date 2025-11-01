ae330e8c0835adc107e564c0cb7c1fd9051335a8
workarea002-cyber <workarea002@gmail.com>
2025-11-01 13:49:56 +0530

    Migrate Sanity backend from JavaScript to TypeScript

---
+++ schemaTypes/index.ts
@@ -0,0 +1,8 @@
+import {about} from './abouts'
+import {contact} from './contact'
+import {experiences} from './experiences'
+import {skills} from './skills'
+import {workExperience} from './workExperience'
+import {works} from './works'
+
+export const schemaTypes = [about, experiences, skills, workExperience, works, contact]
